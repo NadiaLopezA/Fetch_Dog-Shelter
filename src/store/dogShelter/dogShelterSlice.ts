fb5770c478d2715d1faa357ea3e87b11021faabd
Nadia <nbla.escom@gmail.com>
2023-09-14 10:10:31 -0600

    Store and Slices files created

---
+++ src/store/dogShelter/dogShelterSlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const dogShelterSlice = createSlice({
+    name: 'auth',
+    initialState: {
+
+    },
+    reducers: {
+
+    }
+});
+
+export const { } = dogShelterSlice.actions;
