ec5e77afe60d8af81044168abcd359fd957b3bde
Nadia <nbla.escom@gmail.com>
2023-09-19 20:32:09 -0600

    Added default values for env var

--- src/helpers/getEnvVariables.ts
+++ src/helpers/getEnvVariables.ts
@@ -3,7 +3,7 @@
 export const getEnvVariables = () => {
 
     return {
-        VITE_MODE: import.meta.env.VITE_MODE,
-        VITE_API_URL: import.meta.env.VITE_API_URL,
+        VITE_MODE: import.meta.env.VITE_MODE || "dev",
+        VITE_API_URL: import.meta.env.VITE_API_URL || "https://frontend-take-home-service.fetch.com",
     }
 }
